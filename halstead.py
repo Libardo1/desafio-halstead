bb9c20a29a1b45d0046fca339d5c076dad0f8bdb
Tomas Hermosilla <eligos@laptop.(none)>
2013-01-15 00:10:39 -0300

    correcciones a formato de salida

--- halstead.py
+++ halstead.py
@@ -8,19 +8,22 @@ with open(sys.argv[1], 'r') as f:
 with open(sys.argv[2], 'r') as f:
     fuente = f.read()
 
-ops = [len(re.findall("%s " % t, fuente)) for t in tokens]
-n1, N1 = sum([n > 0 for n in ops]), sum(ops)
+n1data = [re.findall(t, fuente) for t in tokens]
+n1data = sum(n1data, [])
+n1, N1 = len(set(n1data)), len(n1data)
 
-ops = [i for i in
+# print (n1data)
+
+n2data = [i for i in
        re.findall(
        '".*"|\'.*\'|[a-zA-Z0-9]*', fuente)
        if i != '' and i not in tokens]
-n2, N2 = len(set(ops)), len(ops)
+n2, N2 = len(set(n2data)), len(n2data)
 
 N, n = N1 + N2 , n1 + n2
 V, D  = N * log(n, 2), (n1 / 2) * (N2 / n2)
 L, E = 1 / D, V * D
 T = E / 18
 
-print("n1 = %f\nn2 = %f\nN1 = %f\nN2 = %f\nN = %f\nn = %f\nV = %f\nD = %f\nL = %f\nE = %f\nT = %f"
-    % (n1, n2, N1, N2, N, n, V, D, L, E, T))
+print("n1 = %d %s\nN1 = %d %s\nn2 = %d %s\nN2 = %d %s\n\nN = %f\nn = %f\nV = %f\nD = %f\nL = %f\nE = %f\nT = %f"
+    % (n1, set(n1data), N1, n1data, n2, set(n2data), N2, n2data, N, n, V, D, L, E, T))

676e2968cce94c0e08a6571499cf7e7b77ec65b7
Tomás Hermosilla <thermosilla@gmail.com>
2013-02-16 13:48:19 -0300

    cambio en forma de extrar operandos y operadores

--- halstead.py
+++ halstead.py
@@ -1,31 +1,25 @@
-import sys, re, math
-
-def getRegexpMatch(regexp, string):
-    data = re.findall(regexp, string)
-    string = re.sub(regexp, ' ', string)
-    return data, string
+import sys
+import re
+import math
 
 
 tokens, fuente = open(
     sys.argv[1], 'r').read().split(), open(sys.argv[2], "r").read()
-tokens.sort(key=lambda s: 1 / len(s))
 
+strregexp = r"\'([^\"\'\\]*(?:\\.[^\"\'\\]*)*)\'|\"([^\"\'\\]*(?:\\.[^\"\'\\]*)*)\""
 
-n2data, fuente = getRegexpMatch(
-    r"\'([^\"\'\\]*(?:\\.[^\"\'\\]*)*)\'|\"([^\"\'\\]*(?:\\.[^\"\'\\]*)*)\"", fuente)
+n2data = re.findall(strregexp, fuente)
 
 fuente = re.sub('|'.join([re.escape(
-    i) for i in ". , ; : ? ! [ ] ( ) { }".split(' ') if i not in tokens]), " ", fuente)
+    i) for i in ". , ; : ? ! [ ] ( ) { }".split(' ') if i not in tokens]), " ", re.sub(
+    strregexp, ' ', fuente))
 
-for i in re.findall('\\w+', fuente):
-    if i not in tokens:
-        data, fuente = getRegexpMatch(r'(?:\b)%s(?:\b)' % re.escape(i), fuente)
-        n2data = n2data + data
+fuente = [i for i in sum([re.split('[a-zA-Z0-9 ]+', j) for j in fuente.splitlines(
+)], []) if i] + [i for i in sum([re.split('\\W+', j) for j in fuente.splitlines()], []) if i]
 
-n1data = []
-for i in tokens:
-    data, fuente = getRegexpMatch(re.escape(i), fuente)
-    n1data = n1data + data
+
+n1data = [i for i in fuente if i in tokens]
+n2data = n2data + [i for i in fuente if i not in tokens]
 
 n1, N1, n2, N2 = len(set(n1data)), len(n1data), len(set(n2data)), len(n2data)
 
@@ -34,5 +28,7 @@ V, D = N * math.log(n, 2), (n1 / 2) * (N2 / n2)
 L, E = 1 / D, V * D
 T = E / 18
 
-print("n1 = %d => %s\nN1 = %d => %s\nn2 = %d => %s\nN2 = %d => %s\n\nN = %d\nn = %d\nV = %f\nD = %f\nL = %f\nE = %f\nT = %f"
-      % (n1, set(n1data), N1, n1data, n2, set(n2data), N2, n2data, N, n, V, D, L, E, T))
+print(
+    "n1 = {} => {}\nN1 = {} => {}\nn2 = {} => {}\nN2 = {} => {}\n\nN = {}\nn = {}\nV = {}\nD = {}\nL = {}\nE = {}\nT = {}".format(
+      n1, set(n1data), N1, n1data, n2, set(n2data), N2, n2data, N, n, V, D, L,
+      E, T))
